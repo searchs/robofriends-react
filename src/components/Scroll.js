0f1992215025990fbf32c38ab5f82640958406ad
Ola Ajibode <ola.ajibode@gmail.com>
2022-03-08 08:23:57 +0000

    Feat: Publish to GH pages

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Scroll = (props) => {
   return (
     <div
-      style={{ overflow: 'scroll', border: '5px solid black', height: '800px' }}
+      style={{
+        overflow: 'scroll',
+        border: '5px solid black',
+        height: '800px',
+        width: '80vw'
+      }}
     >
       {props.children}
     </div>
