369f5d1b1e23e94bb815abfdc14de7161f813633
Ola Ajibode <ola.ajibode@gmail.com>
2022-06-07 10:26:59 +0100

    Feat: Modules update and action

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,27 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  };
+};
+
 function App() {
-  const [robots, setRobots] = useState([]);
-  const [searchField, setSearchField] = useState('');
+  // const [robots, setRobots] = useState([]);
+  // const [searchField, setSearchField] = useState('');
+
+  const { robots } = this.state;
+  const { searchField, onSearchChange } = this.props;
 
   useEffect((props) => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => {
-        setRobots(users);
+        this.setState({ robots: users });
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  const onSearchChange = (event) => {
-    setSearchField(event.target.value);
-  };
+  // const onSearchChange = (event) => {
+  //   setSearchField(event.target.value);
+  // };
 
   const filteredRobots = robots.filter((robots) => {
     return robots.name.toLowerCase().includes(searchField.toLowerCase());
@@ -31,7 +48,7 @@ function App() {
     <h2 className='f2 light-red'>loading...</h2>
   ) : (
     <div className='tc'>
-      <h2 className='f2 dark-blue ttu'>RoboFriends</h2>
+      <h2 className='f2 dark-blue font-sans'>RoboFriends</h2>
       <SearchBox searchChange={onSearchChange} />
       <Scroll>
         <ErrorBoundary>
@@ -42,4 +59,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { searchRobots } from './reducers';
 import 'tachyons';
 import App from './containers/App';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App store={store} />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
